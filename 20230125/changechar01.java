a8cc62a7216c8316291ad4086aa008644ccf0322
筱悠意 <1799820231@qq.com>
2023-01-25 22:50:43 +0800

    java p30

---
+++ 20230125/changechar01.java
@@ -0,0 +1,10 @@
+import java.io.UnsupportedEncodingException;
+
+public class changechar01 {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println("书名\t作者\t\t价格\t销量\t\n三国\t罗贯中\t120\t1000");
+        System.out.println("11111111");
+        System.out.println("作\t1\t1");
+        System.out.println("作者的\t1\t1");
+    }
+}
---
+++ 20230125/changechar02.java
@@ -0,0 +1,5 @@
+public class changechar02 {
+    public static void main(String[] args){
+        System.out.println("姓名\t付佳豪\t\n性别\t男\t\n籍贯\t山西省\t\n地址\t山西省");
+    }
+}
